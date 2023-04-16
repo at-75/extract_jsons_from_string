255fac6c988f9c161fe7b6b35a02613f7f006f7b
at-75 <abhishekonlythakur@gmail.com>
2023-04-17 08:16:24 +0900

    ++correct impl of func

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,23 +1,31 @@
 // use std::{collections::VecDeque, slice::range};
-fn extract(s: String) -> Vec<i32>{
+fn extract(s: String) -> Vec<String>{
     let n = s.len();
-    let m: usize = 0;
-    let v   = vec![1, 2, 3];
-    for i in 0..n{
+    let mut m: usize = 0;
+    let mut v : Vec<String>   = vec![];
+    println!("{}",n);
+    for mut i in 0..n{
         m=i;
         if s.chars().nth(i)==Some('{') {
+           let first_bracket_index=i;
+           let mut second_bracket_index=i;
            let mut score=1;
-           while score>0 {
+           while score>0 && m<n {
                 m+=1;
-                if s.chars().nth(i)==Some('{') {
+                if s.chars().nth(m)==Some('{') {
                     score+=1;
                 }
-                else if s.chars().nth(i)==Some('}'){
+                else if s.chars().nth(m)==Some('}'){
                     score-=1;
                 }
+                i=m;
+                second_bracket_index=i+1;
            }
+           v.push(s.get(first_bracket_index..second_bracket_index).unwrap().to_string());
         }
-        
+    }
+    for s in &v {
+        println!("{}", s);
     }
     return v;
 }
