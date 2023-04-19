12c7cdd20df4faf1cd0e29719e274a966b036636
Kuntipatidar <kuntipatidar1997@gmail.com>
2023-04-19 12:39:35 +0900

    uploading python file

--- python/main.py
+++ python/main.py
@@ -1,1 +1,29 @@
-print("test")
+json='''quygkhbjh{
+"kp": "girl",
+  "at": "boy"
+  "check":{
+    "a":"t"
+  }
+}{"a":"q"}{"s":"x"}ihbljnk'''
+a=0
+arr=[]
+start=0
+end=0;
+json=json.replace("\n","").replace(" ","")
+start=-1
+for i in range (len(json)):
+    if json[i]=='{' and a==0:
+        a+=1
+        start=i
+    elif json[i] == '{':
+        a+=1
+    elif json[i]=='}': 
+        a-=1  
+        if a==0:
+            end=i
+            arr.append(json[start:end+1])
+        
+    
+    
+
+print(arr)
